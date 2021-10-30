3bab17ca7c865d5159fa663cbf5f32ba5a8fd30d
Vladimir <vladimir.fedeniuc@gmail.com>
2021-10-30 18:51:34 +0300

    Добавлена адаптация первого экрана до 768px, сырая версия

--- src/js/script.js
+++ src/js/script.js
@@ -28,4 +28,23 @@ $(document).ready(function(){
   });
 });
 
+// Hamburger
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu'),
+  menuItem = document.querySelectorAll('.menu_item'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('menu_active');
+      })
+  })
+})
 
